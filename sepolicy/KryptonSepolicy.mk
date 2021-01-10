a83e813876f1c353378d4415b8dd6124a2e8d0c3
jhonboy121 <alfredmathew05@gmail.com>
2021-01-10 07:47:29 +0000

    sepolicy: classify as public,private and vendor type
    fixes compilation errors cuased by unknown types and other stuff

--- sepolicy/KryptonSepolicy.mk
+++ sepolicy/KryptonSepolicy.mk
@@ -1,2 +1,9 @@
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-	vendor/krypton/sepolicy/common
+	vendor/krypton/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+	vendor/krypton/sepolicy/common/private
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+	vendor/krypton/sepolicy/common/dynamic \
+	vendor/krypton/sepolicy/common/vendor
