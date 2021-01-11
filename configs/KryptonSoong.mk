9ad237ffb529cf52b2fb3209db82e7b81916feb3
jhonboy121 <alfredmathew05@gmail.com>
2021-01-11 09:04:20 +0000

    krypton: add soong config for rmnetctl

--- configs/KryptonSoong.mk
+++ configs/KryptonSoong.mk
@@ -5,9 +5,17 @@ SOONG_CONFIG_kryptonGlobalVars += \
     target_init_vendor_lib \
     target_surfaceflinger_fod_lib
 
+
+SOONG_CONFIG_NAMESPACES += kryptonQcomVars
+SOONG_CONFIG_kryptonQcomVars += \
+    uses_pre_uplink_features_netmgrd
+
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+
+# Soong bool variables
+SOONG_CONFIG_kryptonQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Soong value variables
 SOONG_CONFIG_kryptonGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
