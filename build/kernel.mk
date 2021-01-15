078a1c607e0ac461a14f041779fac3d09d7b8bd9
jhonboy121 <alfredmathew05@gmail.com>
2021-01-15 12:51:48 +0000

    krypton: introduce kernel.mk for kernel builds
    based on kernel/msm-4.14/AndroidKernel.mk
    also included additional configs in KryptonKernel.mk

---
+++ build/kernel.mk
@@ -0,0 +1,68 @@
+define make-common
+$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(BUILD_ROOT_LOC)$(1) $(KERNEL_MAKE_ENV) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) CROSS_COMPILE_ARM32=$(KERNEL_CROSS_COMPILE_ARM32) $(cc) $(2)
+endef
+
+define make-default
+$(call make-common,$(KERNEL_OUT),$(1))
+endef
+
+$(KERNEL_USR): $(KERNEL_HEADERS_INSTALL)
+	rm -rf $(KERNEL_SYMLINK)
+	ln -s $(TARGET_KERNEL_SOURCE) $(KERNEL_SYMLINK)
+
+$(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_USR)
+
+$(KERNEL_OUT):
+	mkdir -p $(KERNEL_OUT)
+
+$(KERNEL_CONFIG): $(KERNEL_OUT)
+	$(call make-default,$(KERNEL_DEFCONFIG))
+	$(hide) if [ ! -z "$(KERNEL_CONFIG_OVERRIDE)" ]; then \
+			echo "Overriding kernel config with '$(KERNEL_CONFIG_OVERRIDE)'"; \
+			echo $(KERNEL_CONFIG_OVERRIDE) >> $(KERNEL_OUT)/.config; \
+			$(call make-default,oldconfig); fi
+
+$(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_HEADERS_INSTALL)
+	$(hide) echo "Building kernel..."
+	$(hide) rm -rf $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts
+	$(call make-default,$(KERNEL_CFLAGS))
+
+$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT)
+	$(hide) if [ ! -z "$(KERNEL_HEADER_DEFCONFIG)" ]; then \
+			rm -f $(BUILD_ROOT_LOC)$(KERNEL_CONFIG); \
+			$(call make-default,$(KERNEL_HEADER_DEFCONFIG)); \
+			$(call make-default,headers_install);\
+			if [ -d "$(KERNEL_HEADERS_INSTALL)/include/bringup_headers" ]; then \
+				cp -Rf  $(KERNEL_HEADERS_INSTALL)/include/bringup_headers/* $(KERNEL_HEADERS_INSTALL)/include/ ;\
+			fi ;\
+			fi
+	$(hide) if [ "$(KERNEL_HEADER_DEFCONFIG)" != "$(KERNEL_DEFCONFIG)" ]; then \
+			echo "Used a different defconfig for header generation"; \
+			rm -f $(BUILD_ROOT_LOC)$(KERNEL_CONFIG); \
+			$(call make-default,$(KERNEL_DEFCONFIG)); fi
+	$(hide) if [ ! -z "$(KERNEL_CONFIG_OVERRIDE)" ]; then \
+			echo "Overriding kernel config with '$(KERNEL_CONFIG_OVERRIDE)'"; \
+			echo $(KERNEL_CONFIG_OVERRIDE) >> $(KERNEL_OUT)/.config; \
+			$(call make-default,oldconfig); fi
+
+ifneq ($(BOARD_PREBUILT_DTBOIMAGE),)
+$(BOARD_PREBUILT_DTBOIMAGE): $(DTBTOOL) $(TARGET_PREBUILT_INT_KERNEL)
+	$(hide) echo "Building dtbo"
+	$(hide) rm -rf $(BOARD_PREBUILT_DTBOIMAGE)
+	$(hide) $(DTBTOOL) create $@ --page_size=$(BOARD_KERNEL_PAGESIZE) $(shell find $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts -type f -name "*.dtbo" | sort)
+endif
+
+# RTIC DTS to DTB (if MPGen enabled;
+# and make sure we don't break the build if rtic_mp.dts missing)
+$(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
+	stat $(KERNEL_SYMLINK)/rtic_mp.dts 2>/dev/null >&2 && \
+	$(DTC) -O dtb -o $(RTIC_DTB) -b 1 $(DTC_FLAGS) $(KERNEL_SYMLINK)/rtic_mp.dts || \
+	touch $(RTIC_DTB)
+
+# Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
+$(INSTALLED_DTBIMAGE_TARGET): $(TARGET_PREBUILT_INT_KERNEL) $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
+	$(hide) if [ -d "$(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/" ]; then \
+            cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@; \
+          else \
+            cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom/*.dtb $(RTIC_DTB) > $@; \
+          fi
--- configs/KryptonKernel.mk
+++ configs/KryptonKernel.mk
@@ -23,12 +23,9 @@ endif
 ifeq ($(TARGET_KERNEL_HEADER_ARCH),)
 TARGET_KERNEL_HEADER_ARCH := $(TARGET_KERNEL_ARCH)
 endif
-ifeq ($(TARGET_KERNEL_VERSION),)
-TARGET_KERNEL_VERSION := $(shell echo $(TARGET_KERNEL_SOURCE) | sed 's_kernel/msm-__')
-endif
+
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PREBUILTS_COMMON)/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(PREBUILTS_COMMON)/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-TARGET_KERNEL_DISABLE_DEBUGFS := true
 
 # Build tools
 KERNEL_LLVM_SUPPORT := true
@@ -46,7 +43,6 @@ UFDT_APPLY_OVERLAY := $(HOST_OUT_EXECUTABLES)/ufdt_apply_overlay$(HOST_EXECUTABL
 
 TARGET_KERNEL_MAKE_ENV := DTC_EXT=$(TEMP_TOP)/$(DTC)
 TARGET_KERNEL_MAKE_ENV += DTC_OVERLAY_TEST_EXT=$(TEMP_TOP)/$(UFDT_APPLY_OVERLAY)
-TARGET_KERNEL_MAKE_ENV += CONFIG_BUILD_ARM64_DT_OVERLAY=y
 TARGET_KERNEL_MAKE_ENV += HOSTCC=$(CLANG_TOOLCHAIN)/clang
 TARGET_KERNEL_MAKE_ENV += HOSTAR=$(HOST_GCC_TOOLCHAIN)/x86_64-linux-ar
 TARGET_KERNEL_MAKE_ENV += HOSTLD=$(HOST_GCC_TOOLCHAIN)/x86_64-linux-ld
@@ -61,11 +57,137 @@ ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DTBO)),true)
 BOARD_PREBUILT_DTBOIMAGE := $(PRODUCT_OUT)/prebuilt_dtbo.img
 endif
 
-# Include kernel build makefile from kernel source
-include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
+#Android makefile to build kernel as a part of Android Build
+PERL		= perl
+
+KERNEL_TARGET := $(strip $(INSTALLED_KERNEL_TARGET))
+ifeq ($(KERNEL_TARGET),)
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+endif
+
+ifneq ($(TARGET_KERNEL_APPEND_DTB), true)
+INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/dtb.img
+endif
+
+ifeq ($(strip $(BOARD_KERNEL_SEPARATED_DTBO)),true)
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
+endif
+
+TARGET_KERNEL_MAKE_ENV := $(strip $(TARGET_KERNEL_MAKE_ENV))
+ifeq ($(TARGET_KERNEL_MAKE_ENV),)
+KERNEL_MAKE_ENV :=
+else
+KERNEL_MAKE_ENV := $(TARGET_KERNEL_MAKE_ENV)
+endif
+
+TARGET_KERNEL_ARCH := $(strip $(TARGET_KERNEL_ARCH))
+ifeq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := arm
+else
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+endif
+
+TARGET_KERNEL_HEADER_ARCH := $(strip $(TARGET_KERNEL_HEADER_ARCH))
+ifeq ($(TARGET_KERNEL_HEADER_ARCH),)
+KERNEL_HEADER_ARCH := $(KERNEL_ARCH)
+else
+KERNEL_HEADER_ARCH := $(TARGET_KERNEL_HEADER_ARCH)
+endif
+
+KERNEL_HEADER_DEFCONFIG := $(strip $(KERNEL_HEADER_DEFCONFIG))
+ifeq ($(KERNEL_HEADER_DEFCONFIG),)
+KERNEL_HEADER_DEFCONFIG := $(KERNEL_DEFCONFIG)
+endif
+
+# Force 32-bit binder IPC for 64bit kernel with 32bit userspace
+ifeq ($(KERNEL_ARCH),arm64)
+ifeq ($(TARGET_ARCH),arm)
+KERNEL_CONFIG_OVERRIDE := CONFIG_ANDROID_BINDER_IPC_32BIT=y
+endif
+endif
+
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(strip $(TARGET_KERNEL_CROSS_COMPILE_PREFIX))
+ifeq ($(TARGET_KERNEL_CROSS_COMPILE_PREFIX),)
+KERNEL_CROSS_COMPILE := arm-eabi-
+else
+KERNEL_CROSS_COMPILE := $(TARGET_KERNEL_CROSS_COMPILE_PREFIX)
+KERNEL_CROSS_COMPILE_ARM32 := $(TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX)
+endif
+
+ifeq ($(KERNEL_LLVM_SUPPORT), true)
+  ifeq ($(KERNEL_SD_LLVM_SUPPORT), true)  #Using sd-llvm compiler
+    ifeq ($(shell echo $(SDCLANG_PATH) | head -c 1),/)
+       KERNEL_LLVM_BIN := $(SDCLANG_PATH)/clang
+    else
+       KERNEL_LLVM_BIN := $(shell pwd)/$(SDCLANG_PATH)/clang
+    endif
+  else
+     KERNEL_LLVM_BIN := $(CLANG_TOOLCHAIN)/clang #Using aosp-llvm compiler
+  endif
+endif
+
+KERNEL_GCC_NOANDROID_CHK := $(shell (echo "int main() {return 0;}" | $(KERNEL_CROSS_COMPILE)gcc -E -mno-android - > /dev/null 2>&1 ; echo $$?))
+cc :=
+ifeq ($(KERNEL_LLVM_SUPPORT),true)
+  ifeq ($(KERNEL_ARCH), arm64)
+	cc := CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
+  else
+	cc := CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=arm-linux-gnueabihf
+  endif
+else
+ifeq ($(strip $(KERNEL_GCC_NOANDROID_CHK)),0)
+KERNEL_CFLAGS := KCFLAGS=-mno-android
+endif
+endif
+
+BUILD_ROOT_LOC := $(TEMP_TOP)/
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/$(TARGET_KERNEL_SOURCE)
+KERNEL_SYMLINK := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_USR := $(KERNEL_SYMLINK)/usr
+
+# Add RTIC DTB to dtb.img if RTIC MPGen is enabled.
+# Note: unfortunately we can't define RTIC DTS + DTB rule here as the
+# following variable/ tools (needed for DTS generation)
+# are missing - DTB_OBJS, OBJDUMP, KCONFIG_CONFIG, CC, DTC_FLAGS (the only available is DTC).
+# The existing RTIC kernel integration in scripts/link-vmlinux.sh generates RTIC MP DTS
+# that will be compiled with optional rule below.
+# To be safe, we check for MPGen enable.
+ifdef RTIC_MPGEN
+RTIC_DTB := $(KERNEL_SYMLINK)/rtic_mp.dtb
+endif
+
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+
+ifeq ($(KERNEL_DEFCONFIG)$(wildcard $(KERNEL_CONFIG)),)
+$(error Kernel configuration not defined, cannot build kernel)
+else
+
+ifeq ($(BOARD_KERNEL_IMAGE_NAME),)
+ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image
+else
+ifeq ($(KERNEL_ARCH),arm64)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.gz
+else
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/zImage
+endif
+endif
+ifeq ($(TARGET_KERNEL_APPEND_DTB), true)
+TARGET_PREBUILT_INT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)-dtb
+endif
+else
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
+endif
+
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+
+# Include kernel build makefile
+include vendor/krypton/build/kernel.mk
 $(TARGET_PREBUILT_KERNEL): $(DTC) $(UFDT_APPLY_OVERLAY)
 
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
 
+endif
 endif
