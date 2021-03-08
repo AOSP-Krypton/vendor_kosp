9a37f0c002c07585870a3f04fa08cbfcc4a42bf0
jhonboy121 <alfredmathew05@gmail.com>
2021-03-08 06:04:22 +0000

    Revert "krypton: update magiskinit to v22.0, dropped arm variant"
    * v22 has got some bugs, will update when its stable
    
    This reverts commit 3eef15fafab24387ac9fd3c9a538071a17f5fc83.

--- magisk/Android.mk
+++ magisk/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_MAGISK_INIT),true)
-$(shell cp $(LOCAL_PATH)/magiskinit $(PRODUCT_OUT)/magiskinit)
+$(shell cp $(LOCAL_PATH)/$(TARGET_ARCH)/magiskinit $(PRODUCT_OUT)/magiskinit)
 endif
