89e0cdbb30d5a069f9edb50405681c94073b588d
xplodwild <xplod@ouverta.fr>
2021-07-31 01:20:42 +0530

    krypton: add some basic props
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- configs/KryptonCommon.mk
+++ configs/KryptonCommon.mk
@@ -112,3 +112,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Don't compile SystemUITests
 EXCLUDE_SYSTEMUI_TESTS := true
+
+# Common props
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1
