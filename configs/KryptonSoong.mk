bbc3c31e53e6284516e3c0f405c79a01d9eebde0
Rashed Abdel-Tawab <rashed@linux.com>
2021-10-14 21:57:21 +0530

    krypton: dynamically generate kernel headers
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- configs/KryptonSoong.mk
+++ configs/KryptonSoong.mk
@@ -14,6 +14,29 @@
 
 # Krypton soong configs
 
+# Add variables that we wish to make available to soong here.
+EXPORT_TO_SOONG := \
+    KERNEL_ARCH \
+    KERNEL_CROSS_COMPILE \
+    KERNEL_MAKE_FLAGS \
+    TARGET_KERNEL_CONFIG \
+    TARGET_KERNEL_SOURCE
+
+# Setup SOONG_CONFIG_* vars to export the vars listed above.
+# Documentation here:
+# https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
+
+SOONG_CONFIG_NAMESPACES += kryptonVarsPlugin
+
+SOONG_CONFIG_kryptonVarsPlugin :=
+
+define addVar
+  SOONG_CONFIG_kryptonVarsPlugin += $(1)
+  SOONG_CONFIG_kryptonVarsPlugin_$(1) := $$(subst ",\",$$($1))
+endef
+
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
 SOONG_CONFIG_NAMESPACES += kryptonGlobalVars
 SOONG_CONFIG_kryptonGlobalVars += \
     target_init_vendor_lib \
