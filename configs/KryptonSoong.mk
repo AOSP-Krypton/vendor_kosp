00140a20a621e812b623cf8ff6d9193c6af351c1
Pig <pig.priv@gmail.com>
2021-10-29 18:00:42 +0530

    krypton: soong: add surfaceflinger_qcom_extensions module
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- configs/KryptonSoong.mk
+++ configs/KryptonSoong.mk
@@ -56,7 +56,8 @@ SOONG_CONFIG_kryptonQcomVars += \
     uses_pre_uplink_features_netmgrd \
     supports_hw_fde \
     supports_hw_fde_perf \
-    uses_qti_camera_device
+    uses_qti_camera_device \
+    qcom_display_headers_namespace
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -76,3 +77,4 @@ SOONG_CONFIG_kryptonGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE
 SOONG_CONFIG_kryptonQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_kryptonQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
 SOONG_CONFIG_kryptonQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+SOONG_CONFIG_kryptonQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/display-commonsys-intf
