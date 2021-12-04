08bd724149658c869db961f5d67bcaa0bed91988
jhonboy121 <alfredmathew05@gmail.com>
2021-12-05 03:20:37 +0530

    krypton: relocate icon packs to vendor/themes
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- configs/KryptonCommon.mk
+++ configs/KryptonCommon.mk
@@ -183,36 +183,4 @@ endif
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
-    NavigationBarModeGesturalOverlayFS \
-    IconPackCircularAndroidOverlay \
-	IconPackCircularLauncherOverlay \
-	IconPackCircularSettingsOverlay \
-	IconPackCircularSystemUIOverlay \
-	IconPackCircularThemePickerOverlay \
-	IconPackVictorAndroidOverlay \
-	IconPackVictorLauncherOverlay \
-	IconPackVictorSettingsOverlay \
-	IconPackVictorSystemUIOverlay \
-	IconPackVictorThemePickerOverlay \
-	IconPackSamAndroidOverlay \
-	IconPackSamLauncherOverlay \
-	IconPackSamSettingsOverlay \
-	IconPackSamSystemUIOverlay \
-	IconPackSamThemePickerOverlay \
-	IconPackKaiAndroidOverlay \
-	IconPackKaiLauncherOverlay \
-	IconPackKaiSettingsOverlay \
-	IconPackKaiSystemUIOverlay \
-	IconPackKaiThemePickerOverlay \
-	IconPackFilledAndroidOverlay \
-	IconPackFilledLauncherOverlay \
-	IconPackFilledSettingsOverlay \
-	IconPackFilledSystemUIOverlay \
-	IconPackFilledThemePickerOverlay \
-	IconPackRoundedAndroidOverlay \
-	IconPackRoundedLauncherOverlay \
-	IconPackRoundedSettingsOverlay \
-	IconPackRoundedSystemUIOverlay \
-	IconPackRoundedThemePickerOverlay \
-	IconPackOOSAndroidOverlay \
-	IconPackOOSSystemUIOverlay
+    NavigationBarModeGesturalOverlayFS
