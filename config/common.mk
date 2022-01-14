yaap/vendor_yaap

config: sepolicy: import flipendo/turbo_adapter sepolicy
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -136,3 +136,6 @@ PRODUCT_HOST_PACKAGES += \
 
 # Themes
 $(call inherit-product, vendor/themes/common.mk)
+
+# Sepolicy
+$(call inherit-product, vendor/yaap/config/sepolicy.mk)

AOSP-Krypton/vendor_kosp

config/common.mk
@@ -173,3 +173,5 @@ PRODUCT_PACKAGES += \
 # Themes
 $(call inherit-product, vendor/themes/common.mk)
 
+# Sepolicy
+$(call inherit-product, vendor/krypton/config/sepolicy.mk)